d05334ef00fb2b0bd9688daf72a7ef9455ec6be8
mrandou <mrandou@student.42.fr>
2020-10-18 18:09:00 +0200

    Minor changes

--- src/Style/MakeStyles.js
+++ src/Style/MakeStyles.js
@@ -35,6 +35,7 @@ export const cardStyles = makeStyles({
     margin: '0 auto',
     maxWidth: 280,
     color: "white",
+    textAlign: 'center',
     backgroundColor: Theme.cardColor,
   },
   media: {
